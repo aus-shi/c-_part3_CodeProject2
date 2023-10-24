e11609288a5ba50aca65a7abf8a9b1b2110ec673
AusKruger <firdausam98@gmail.com>
2023-10-24 22:13:52 +0800

    first-commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿string[] roles = { "administrator", "user", "manager" };
+string? roleName;
+bool keep_running = true;
+string? loweredRoleName;
+
+Console.WriteLine("Enter your role: ");
+
+do
+{
+    roleName = Console.ReadLine();
+    loweredRoleName = roleName.ToLower();
+    if (Array.Exists(roles, role => role == loweredRoleName))
+    {
+        Console.WriteLine($"Your input value ({roleName}) has been accepted.");
+        keep_running = false;
+        
+    }
+    else
+    {
+        Console.WriteLine($"The role name that you entered, \"{roleName}\" is not valid. Enter your role name (Administrator, Manager, or User)");
+        do
+        {
+            roleName = Console.ReadLine();
+            loweredRoleName = roleName.ToLower();
+            if (Array.Exists(roles, role => role == loweredRoleName))
+            {
+                Console.WriteLine($"Your input value ({roleName}) has been accepted.");
+                keep_running = false;
+                break;
+            }
+            else
+            {
+                Console.WriteLine($"The role name that you entered, \"{roleName}\" is not valid. Enter your role name (Administrator, Manager, or User)");
+            }
+        } while (true);
+    }
+
+} while (keep_running);
+
+Console.ReadLine();
